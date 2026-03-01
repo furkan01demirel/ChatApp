cf98cbfb5cbadebe9343bece1bc45659aed37046
Furkan Demirel <furkan.demirel.056@gmail.com>
2026-03-01 19:28:56 +0300

    add env

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyActCy9QnEIxab9bpFXnFXoahbmbKuYJuA",
-  authDomain: "chatapp-86b01.firebaseapp.com",
-  projectId: "chatapp-86b01",
-  storageBucket: "chatapp-86b01.firebasestorage.app",
-  messagingSenderId: "1007805478475",
-  appId: "1:1007805478475:web:b205f59c00683ea978ffce",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
